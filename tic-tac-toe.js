7f6f339d942d71043065008a3332e468b2d7765a
sink007 <lhines2000@gmail.com>
2024-10-19 21:37:45 -0500

    exercise 1 complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,8 @@
-// Wait for the DOM to fully load before running the script
 document.addEventListener('DOMContentLoaded', function() {
-    // Select all div elements inside the #board container
     const squares = document.querySelectorAll('#board div');
-    
-    // Loop through each div and add the 'square' class
+
     squares.forEach(function(square) {
       square.classList.add('square');
     });
-  });
+});
   
